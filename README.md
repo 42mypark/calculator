662620aa676f6fd5a18b8afb25d951b162854958
42mypark <skyttl34@gmail.com>
2022-12-19 11:48:09 +0900

    README

---
+++ README.md
@@ -0,0 +1,13 @@
+## Calculator with LR Parser
+
+### Grammar
+
+- E' -> E
+- E -> T + E
+- E -> T - E
+- E -> T
+- T -> F \* T
+- T -> F / T
+- T -> F
+- F -> n
+- F -> ( E )
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,13 +1,3 @@
-// E' -> E
-// E -> T + E
-// E -> T - E
-// E -> T
-// T -> F * T
-// T -> F / T
-// T -> F
-// F -> n
-// F -> ( E )
-
 extern crate alloc;
 
 use alloc::collections::VecDeque;
